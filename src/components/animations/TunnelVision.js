e05af1bad02df4d0d618da70c0187956c437e5f7
Matt Masurka <m.masurka@gmail.com>
2020-04-22 21:35:22 -0700

    D3 working

--- src/components/animations/TunnelVision.js
+++ src/components/animations/TunnelVision.js
@@ -1,30 +1,14 @@
 import React, { useEffect } from "react";
 import { select } from "d3-selection";
-import { line as d3Line } from "d3-shape";
 import { scaleLinear, scaleTime } from "d3-scale";
 
-const xScale = scaleTime(
-  [new Date(2007, 3, 24), new Date(2007, 4, 1)],
-  [0, 100]
-);
-const yScale = scaleLinear([90, 100], [0, 100]);
+// const xScale = scaleTime(
+//   [new Date(2007, 3, 24), new Date(2007, 4, 1)],
+//   [0, 100]
+// );
+// const yScale = scaleLinear([90, 100], [0, 100]);
 
-var data = [
-  { date: new Date(2007, 3, 24), value: 93.24 },
-  { date: new Date(2007, 3, 25), value: 95.35 },
-  { date: new Date(2007, 3, 26), value: 98.84 },
-  { date: new Date(2007, 3, 27), value: 99.92 },
-  { date: new Date(2007, 3, 30), value: 99.8 },
-  { date: new Date(2007, 4, 1), value: 99.47 },
-];
-
-var line = d3Line()
-  .x(function(d) {
-    return xScale(d.date);
-  })
-  .y(function(d) {
-    return yScale(d.value);
-  });
+const canvasSize = 500;
 
 const TunnelVision = () => {
   useEffect(() => {
@@ -34,23 +18,20 @@ const TunnelVision = () => {
   const draw = () => {
     select(".tunnel-wrapper")
       .append("svg")
-      .attr("width", 500)
-      .attr("height", 500)
+      .attr("width", canvasSize)
+      .attr("height", canvasSize)
       .append("g")
-      .attr("transform", "translate(20,20)")
-      .append("path")
-      .attr("stroke-width", 1)
+      .append("rect")
+      .attr("transform-origin", `${canvasSize / 2}px ${canvasSize / 2}px`)
+      .attr("transform", `center`)
+      .attr("width", canvasSize)
+      .attr("height", canvasSize)
       .attr("stroke", "black")
-      .attr("fill", "none")
-      .attr("d", line(data));
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
   };
 
-  return (
-    <div
-      class="tunnel-wrapper"
-      // style={{ transform: "scale(1)" }}
-    />
-  );
+  return <div class="tunnel-wrapper" />;
 };
 
 export default TunnelVision;
