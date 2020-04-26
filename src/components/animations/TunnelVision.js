c46972a5757dd5ae735b3baebcda330e82665750
Matthew Masurka <m.masurka@gmail.com>
2020-04-25 23:54:28 -0700

    Animation loop is working but still need to calculate x & y positions

--- src/components/animations/TunnelVision.js
+++ src/components/animations/TunnelVision.js
@@ -1,36 +1,82 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { select } from "d3-selection";
-import { transition } from "d3-transition";
 import { scaleLinear, scalePow } from "d3-scale";
-import { easeLinear } from "d3-ease";
 
 const canvasSize = 400;
 const halfCanvas = canvasSize / 2;
-const squareCount = 40;
+const squareCount = 30;
 const sizeScale = scalePow()
   .exponent(6)
   .domain([0, squareCount])
   .range([0, canvasSize]);
-const alphaScale = scalePow()
-  .exponent(6)
+const opacityScale = scalePow()
+  .exponent(3)
   .domain([0, squareCount])
   .range([0.5, 1]);
 const strokeWidthScale = scalePow()
   .exponent(6)
   .domain([0, squareCount])
   .range([0.5, 2]);
+const xMouseScale = scaleLinear()
+  .domain([0, window.innerWidth])
+  .range([0, canvasSize]);
+const yMouseScale = scaleLinear()
+  .domain([0, window.innerHeight])
+  .range([0, canvasSize]);
 const squares = [];
+const framesPerLoop = 60;
+let currentFrame = 0;
 
-for (let i = 0; i < squareCount; i++) {
+// create squares
+for (let i = 0; i <= squareCount; i++) {
   const square = {
-    size: sizeScale(i),
-    alpha: alphaScale(i),
-    strokeWidth: strokeWidthScale(i),
-    x: halfCanvas - sizeScale(i) / 2,
-    y: halfCanvas - sizeScale(i) / 2
+    size: [sizeScale(i)],
+    opacity: [opacityScale(i)],
+    strokeWidth: [strokeWidthScale(i)],
+    x: [{ value: halfCanvas - sizeScale(i) / 2, scaleFn: () => {} }],
+    y: [{ value: halfCanvas - sizeScale(i) / 2, scaleFn: () => {} }]
   };
   squares.push(square);
 }
+
+// create values for each frame
+for (let frame = 1; frame < framesPerLoop; frame++) {
+  squares.forEach((square, sqIdx) => {
+    if (sqIdx < squares.length - 1) {
+      square.x[frame] = {
+        value:
+          ((squares[sqIdx + 1].x[0].value - square.x[0].value) /
+            framesPerLoop) *
+            frame +
+          square.x[0].value,
+        scaleFn: () => {}
+      };
+      square.y[frame] = {
+        value:
+          ((squares[sqIdx + 1].y[0].value - square.y[0].value) /
+            framesPerLoop) *
+            frame +
+          square.y[0].value,
+        scaleFn: () => {}
+      };
+      square.size[frame] =
+        ((squares[sqIdx + 1].size[0] - square.size[0]) / framesPerLoop) *
+          frame +
+        square.size[0];
+      square.opacity[frame] =
+        ((squares[sqIdx + 1].opacity[0] - square.opacity[0]) / framesPerLoop) *
+          frame +
+        square.opacity[0];
+      square.strokeWidth[frame] =
+        ((squares[sqIdx + 1].strokeWidth[0] - square.strokeWidth[0]) /
+          framesPerLoop) *
+          frame +
+        square.strokeWidth[0];
+    }
+  });
+}
+
+console.log(squares);
 
 const TunnelVision = () => {
   let wrapper;
@@ -47,46 +93,46 @@ const TunnelVision = () => {
       mouseCoords.y = e.clientY;
     });
 
-    draw();
+    requestAnimationFrame(draw);
   }, []);
 
   const draw = () => {
-    const t = transition()
-      .duration(5000)
-      .ease(easeLinear);
+    // console.log(mouseCoords.x, mouseCoords.y);
+    if (currentFrame < framesPerLoop) {
+      wrapper
+        .selectAll("rect")
+        .data(squares)
+        .join("rect")
+        .attr("stroke", "black")
+        .attr("fill", "none")
+        .attr("opacity", (d, i) =>
+          i < squares.length - 1 ? squares[i].opacity[currentFrame] : 1
+        )
+        .attr("width", (d, i) =>
+          i < squares.length - 1 ? squares[i].size[currentFrame] : canvasSize
+        )
+        .attr("height", (d, i) =>
+          i < squares.length - 1 ? squares[i].size[currentFrame] : canvasSize
+        )
+        .attr("stroke-width", (d, i) =>
+          i < squares.length - 1 ? squares[i].strokeWidth[currentFrame] : 4
+        )
+        .attr("x", (d, i) =>
+          i < squares.length - 1
+            ? squares[i].x[currentFrame].value + xMouseScale(mouseCoords.x)
+            : 0
+        )
+        .attr("y", (d, i) =>
+          i < squares.length - 1
+            ? squares[i].y[currentFrame].value + yMouseScale(mouseCoords.y)
+            : 0
+        );
+      currentFrame++;
+    } else {
+      currentFrame = 0;
+    }
 
-    wrapper
-      .selectAll("rect")
-      .data(squares)
-      .join("rect")
-      .attr("x", d => d.x)
-      .attr("y", d => d.y)
-      .attr("opacity", d => d.alpha)
-      .attr("width", d => d.size)
-      .attr("height", d => d.size)
-      .attr("stroke", "black")
-      .attr("stroke-width", d => d.strokeWidth)
-      .attr("fill", "none")
-      .transition(t)
-      .attr("opacity", (d, i) =>
-        i < squares.length - 1 ? squares[i + 1].opacity : 1
-      )
-      .attr("x", (d, i) =>
-        i < squares.length - 1 ? halfCanvas - squares[i + 1].size / 2 : 0
-      )
-      .attr("y", (d, i) =>
-        i < squares.length - 1 ? halfCanvas - squares[i + 1].size / 2 : 0
-      )
-      .attr("width", (d, i) =>
-        i < squares.length - 1 ? squares[i + 1].size : canvasSize
-      )
-      .attr("height", (d, i) =>
-        i < squares.length - 1 ? squares[i + 1].size : canvasSize
-      )
-      .attr("stroke-width", (d, i) =>
-        i < squares.length - 1 ? squares[i + 1].strokeWidth : d.strokeWidth
-      )
-      .on("end", draw);
+    requestAnimationFrame(draw);
   };
 
   return <div className="tunnel-wrapper" />;
